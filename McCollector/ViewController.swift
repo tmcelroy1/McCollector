e1b6c780f372a3ffa542f196294522a27f446e30
Tim McElroy1 <tmcelroyt@mac.com>
2017-06-29 12:20:34 -0400

    First save no data

---
+++ McCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  McCollector
+//
+//  Created by Tim McElroy1 on 6/29/17.
+//  Copyright © 2017 Tim McElroy1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
