f42b541706f3245560b199903e22c08ff075873d
Tim McElroy1 <tmcelroyt@mac.com>
2017-06-29 15:00:34 -0400

    2nd View controller added with buttons and connections made

---
+++ McCollector/CollectionAddViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionAddViewController.swift
+//  McCollector
+//
+//  Created by Tim McElroy1 on 6/29/17.
+//  Copyright © 2017 Tim McElroy1. All rights reserved.
+//
+
+import UIKit
+
+class CollectionAddViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UIImageView!
+    
+    @IBOutlet weak var descriptTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func photoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
