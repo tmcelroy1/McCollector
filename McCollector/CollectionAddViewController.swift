4dded7b596a0005cabc4c28fe5b7a5bf8e275197
Tim McElroy1 <tmcelroyt@mac.com>
2017-06-29 21:46:53 -0400

    Getting images for Photo Library

--- McCollector/CollectionAddViewController.swift
+++ McCollector/CollectionAddViewController.swift
@@ -8,22 +8,38 @@
 
 import UIKit
 
-class CollectionAddViewController: UIViewController {
+class CollectionAddViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var collectionView: UIImageView!
-    
     @IBOutlet weak var descriptTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func photoTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+       let image =  info[UIImagePickerControllerOriginalImage] as! UIImage
+        collectionView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func addTapped(_ sender: Any) {
